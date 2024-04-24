1b5a1f207b7986297734766e6b639bfb5fcb5e48
JuanFC25 <juankaprin@gmail.com>
2024-04-23 21:10:56 -0300

    commit inicial

---
+++ src/test/java/isi/dan/mspago/MsPagoApplicationTests.java
@@ -0,0 +1,13 @@
+package isi.dan.mspago;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsPagoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
